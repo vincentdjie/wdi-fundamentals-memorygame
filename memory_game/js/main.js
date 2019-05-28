776d05f7bb4bc7667671413c41da2e31ca6e7124
Vincent Renaldo Jita <daejaeblaq@Vincents-MacBook-Pro.local>
2019-05-27 18:36:42 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,22 @@
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cards.push(cardOne);
+console.log("User flipped" + cardOne);
+
+var cardTwo = cards[2];
+
+cards.push(cardTwo);
+console.log("User flipped" + cardTwo);
+
+if (cardsinPlay.length === 2) {
+	if (cardsinPlay[0] === cardsinPlay[1]) {
+		console.log("You found a match!");
+		else 
+			console.log("Sorry, try again.");
+	}
+}
+
+
